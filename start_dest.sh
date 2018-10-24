3f4b4904ffd223434e3683a144d4ddc96fb49642
urabe <urabe0225@gmail.com>
2018-10-24 10:56:59 +0900

    modified shellscripts

--- start_dest.sh
+++ start_dest.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-nohup python ./dest_button.py --host 'mqtt.tech-sketch.jp' --port 8883 --ssl --key_file './ssl/DST_Root_CA_X3.pem' --device_id dest_human_sensor_0000000000000001 &
-nohup python ./dest_led.py --host 'mqtt.tech-sketch.jp' --port 8883 --ssl --key_file './ssl/DST_Root_CA_X3.pem' --device_id dest_led_0000000000000001 &
+nohup /usr/bin/python3 /home/pi/ogc-poc1-device/dest_button.py --host 'mqtt.tech-sketch.jp' --port 8883 --ssl --key_file '/home/pi/ogc-poc1-device/ssl/DST_Root_CA_X3.pem' --device_id dest_human_sensor_000000000000000X &
+nohup sudo /usr/bin/python3 /home/pi/ogc-poc1-device/dest_led.py --host 'mqtt.tech-sketch.jp' --port 8883 --ssl --key_file '/home/pi/ogc-poc1-device/ssl/DST_Root_CA_X3.pem' --device_id dest_led_000000000000000X &
--- start_ent.sh
+++ start_ent.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-nohup python ./entrance_button.py --host 'mqtt.tech-sketch.jp' --port 8883 --ssl --key_file './ssl/DST_Root_CA_X3.pem' --device_id button_sensor_0000000000000001 &
+nohup /usr/bin/python3 /home/pi/ogc-poc1-device/entrance_button.py --host 'mqtt.tech-sketch.jp' --port 8883 --ssl --key_file '/home/pi/ogc-poc1-device/ssl/DST_Root_CA_X3.pem' --device_id button_sensor_000000000000000X &
